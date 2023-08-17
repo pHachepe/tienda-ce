0fc38e4f4a1e0d220cffeb9ee3d0ab1d42860c3a
PeHachePe <pehachepe@uoc.edu>
2023-08-18 00:39:35 +0200

    prueba ssl

--- api/config/db.php
+++ api/config/db.php
@@ -26,7 +26,25 @@ if ($_SERVER['SERVER_NAME'] === 'localhost') {
 
     $conn = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
 } else {
-    $conn = new mysqli($_ENV['TIDB_HOST'], $_ENV['TIDB_USER'], $_ENV['TIDB_PASSWORD'], $_ENV['TIDB_NAME'], $_ENV['TIDB_PORT']);
+    //$conn = new mysqli($_ENV['TIDB_HOST'], $_ENV['TIDB_USER'], $_ENV['TIDB_PASSWORD'], $_ENV['TIDB_NAME'], $_ENV['TIDB_PORT']);
+    // connect with ssl in tidb
+    $conn->ssl_set(
+        NULL,
+        NULL,
+        '/etc/mysql/certs/ca.pem',
+        NULL,
+        NULL
+    );
+    $conn->real_connect(
+        $_ENV['TIDB_HOST'],
+        $_ENV['TIDB_USER'],
+        $_ENV['TIDB_PASSWORD'],
+        $_ENV['TIDB_NAME'],
+        $_ENV['TIDB_PORT'],
+        NULL,
+        MYSQLI_CLIENT_SSL
+    );
+
 }
 
 if ($conn->connect_error) {
