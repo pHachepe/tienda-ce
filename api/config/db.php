57247108148ac74d5dd881dd88099be30060e882
PeHachePe <pehachepe@uoc.edu>
2023-08-18 00:38:04 +0200

    prueba

--- api/config/db.php
+++ api/config/db.php
@@ -1,10 +1,29 @@
 <?php
 echo "SERVER_NAME: " . $_SERVER['SERVER_NAME'] . "<br>";
-echo "TIDB_NAME: " . $_ENV['TIDB_NAME'] . "<br>";
 if ($_SERVER['SERVER_NAME'] === 'localhost') {
     require __DIR__ . '/../vendor/autoload.php'; // Ruta correcta al archivo autoload.php de Composer
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../..'); // Ruta correcta al archivo .env
     $dotenv->load();
+    // mysqli with ssl
+    $mysql = mysqli_init();
+    $mysql->ssl_set(
+        NULL,
+        NULL,
+        '/etc/mysql/certs/ca.pem',
+        NULL,
+        NULL
+    );
+    $conn = mysqli_real_connect(
+        $mysql,
+        $_ENV['DB_HOST'],
+        $_ENV['DB_USER'],
+        $_ENV['DB_PASS'],
+        $_ENV['DB_NAME'],
+        $_ENV['DB_PORT'],
+        NULL,
+        MYSQLI_CLIENT_SSL
+    );
+
     $conn = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
 } else {
     $conn = new mysqli($_ENV['TIDB_HOST'], $_ENV['TIDB_USER'], $_ENV['TIDB_PASSWORD'], $_ENV['TIDB_NAME'], $_ENV['TIDB_PORT']);
