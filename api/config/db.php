113e7308852fdcc468f7a33d0921caf2c9954c35
PeHachePe <pehachepe@uoc.edu>
2023-08-18 00:24:14 +0200

    add tidb conn

--- api/config/db.php
+++ api/config/db.php
@@ -6,7 +6,7 @@ if ($_SERVER['SERVER_NAME'] === 'localhost') {
     $dotenv->load();
     $conn = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
 } else {
-    $conn = new mysqli($_ENV['POSTGRES_HOST'], $_ENV['POSTGRES_USER'], $_ENV['POSTGRES_PASS'], $_ENV['POSTGRES_NAME'], $_ENV['POSTGRES_PORT']);
+    $conn = new mysqli($_ENV['TIDB_HOST'], $_ENV['TIDB_USER'], $_ENV['TIDB_PASSWORD'], $_ENV['TIDB_NAME'], $_ENV['TIDB_PORT']);
 }
 
 if ($conn->connect_error) {
