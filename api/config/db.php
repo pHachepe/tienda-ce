59c174151cce8d0ed79181336bf7f026c7c04f96
PeHachePe <pehachepe@uoc.edu>
2023-08-17 23:39:39 +0200

    update prod connection

--- api/config/db.php
+++ api/config/db.php
@@ -1,17 +1,14 @@
 <?php
+echo "SERVER_NAME: " . $_SERVER['SERVER_NAME'] . "<br>";
 if ($_SERVER['SERVER_NAME'] === 'localhost') {
     require __DIR__ . '/../vendor/autoload.php'; // Ruta correcta al archivo autoload.php de Composer
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..'); // Ruta correcta al archivo .env
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../..'); // Ruta correcta al archivo .env
     $dotenv->load();
+    $conn = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
+} else {
+    $conn = new mysqli($_ENV['POSTGRES_HOST'], $_ENV['POSTGRES_USER'], $_ENV['POSTGRES_PASS'], $_ENV['POSTGRES_NAME'], $_ENV['POSTGRES_PORT']);
 }
-echo "host: " . $_ENV['DB_HOST'] . "<br>";
-echo "user: " . $_ENV['DB_USER'] . "<br>";
-echo "pass: " . $_ENV['DB_PASS'] . "<br>";
-echo "name: " . $_ENV['DB_NAME'] . "<br>";
-echo "port: " . $_ENV['DB_PORT'] . "<br>";
-echo "server: " . $_SERVER['SERVER_NAME'] . "<br>";
 
-$conn = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
