46bb7d7eb7462d1e9171529d95c35bef42b8f536
PeHachePe <pehachepe@uoc.edu>
2023-08-18 19:53:45 +0200

    db

--- api/config/db.php
+++ api/config/db.php
@@ -1,7 +1,7 @@
 <?php
 if ($_SERVER['SERVER_NAME'] === 'localhost') {
-    require __DIR__ . '/../vendor/autoload.php';
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../..');
+    require __DIR__ . '/../vendor/autoload.php'; // Ruta correcta al archivo autoload.php de Composer
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../..'); // Ruta correcta al archivo .env
     $dotenv->load();
 }
 $servername = $_ENV['TIDB_HOST'];
@@ -12,9 +12,7 @@ $password = $_ENV['TIDB_PASSWORD'];
 // Create connection
 $conn = mysqli_init();
 //mysqli_ssl_set($conn, NULL, NULL, "/etc/ssl/cert.pem", NULL, NULL);
-
-    mysqli_real_connect($conn, $servername, $username, $password, $dbname);
-
+mysqli_real_connect($conn, $servername, $username, $password, $dbname, 4000, NULL, MYSQLI_CLIENT_SSL);
 
 // Check connection
 if (!$conn) {
