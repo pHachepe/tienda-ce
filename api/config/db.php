76017d5dc51850712ec2558f99a0f663215ee32e
PeHachePe <pehachepe@uoc.edu>
2023-08-18 20:03:56 +0200

    db

--- api/config/db.php
+++ api/config/db.php
@@ -1,5 +1,7 @@
 <?php
-if ($_SERVER['SERVER_NAME'] === 'localhost') {
+$isLocal = $_SERVER['SERVER_NAME'] === 'localhost';
+if ($isLocal) {
+    echo "es local";
     require __DIR__ . '/../vendor/autoload.php';
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../..');
     $dotenv->load();
@@ -12,14 +14,11 @@ $password = $_ENV['TIDB_PASSWORD'];
 // Create connection
 $conn = mysqli_init();
 //mysqli_ssl_set($conn, NULL, NULL, "/etc/ssl/cert.pem", NULL, NULL);
-//if (!$_SERVER['SERVER_NAME'] === 'localhost') {
+if ($isLocal) {
+    mysqli_real_connect($conn, $servername, $username, $password, $dbname);
+} else {
     mysqli_real_connect($conn, $servername, $username, $password, $dbname, 4000, NULL, MYSQLI_CLIENT_SSL);
-//} else {
-//    mysqli_real_connect($conn, $servername, $username, $password, $dbname);
-//}
-echo "server: " . $_SERVER['SERVER_NAME'];
-$isLocal = $_SERVER['SERVER_NAME'] === 'localhost';
-echo "es el server local? " . $isLocal;
+}
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
