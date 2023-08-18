ae22cd5251e05aa54b257b08a6b439a77d3f1c96
PeHachePe <pehachepe@uoc.edu>
2023-08-18 13:28:30 +0200

    update conection db

--- api/config/db.php
+++ api/config/db.php
@@ -1,53 +1,19 @@
 <?php
-echo "SERVER_NAME: " . $_SERVER['SERVER_NAME'] . "<br>";
-if ($_SERVER['SERVER_NAME'] === 'localhost') {
-    require __DIR__ . '/../vendor/autoload.php'; // Ruta correcta al archivo autoload.php de Composer
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../..'); // Ruta correcta al archivo .env
-    $dotenv->load();
-    // mysqli with ssl
-    $mysql = mysqli_init();
-    $mysql->ssl_set(
-        NULL,
-        NULL,
-        '/etc/mysql/certs/ca.pem',
-        NULL,
-        NULL
-    );
-    $conn = mysqli_real_connect(
-        $mysql,
-        $_ENV['DB_HOST'],
-        $_ENV['DB_USER'],
-        $_ENV['DB_PASS'],
-        $_ENV['DB_NAME'],
-        $_ENV['DB_PORT'],
-        NULL,
-        MYSQLI_CLIENT_SSL
-    );
+require __DIR__ . '/../vendor/autoload.php'; // Ruta correcta al archivo autoload.php de Composer
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../..'); // Ruta correcta al archivo .env
+$dotenv->load();
 
-    $conn = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
-} else {
-    //$conn = new mysqli($_ENV['TIDB_HOST'], $_ENV['TIDB_USER'], $_ENV['TIDB_PASSWORD'], $_ENV['TIDB_NAME'], $_ENV['TIDB_PORT']);
-    // connect with ssl in tidb
-    $conn = mysqli_init();
-    $conn->ssl_set(
-        NULL,
-        NULL,
-        '/etc/mysql/certs/ca.pem',
-        NULL,
-        NULL
-    );
-    $conn->real_connect(
-        $_ENV['TIDB_HOST'],
-        $_ENV['TIDB_USER'],
-        $_ENV['TIDB_PASSWORD'],
-        $_ENV['TIDB_NAME'],
-        $_ENV['TIDB_PORT'],
-        NULL,
-        MYSQLI_CLIENT_SSL
-    );
+$servername = $_ENV['TIDB_HOST'];
+$dbname = $_ENV['TIDB_NAME'];
+$username = $_ENV['TIDB_USER'];
+$password = $_ENV['TIDB_PASSWORD'];
 
-}
+// Create connection
+$conn = mysqli_init();
+//mysqli_ssl_set($conn, NULL, NULL, "/etc/ssl/cert.pem", NULL, NULL);
+mysqli_real_connect($conn, $servername, $username, $password, $dbname, 4000, NULL, MYSQLI_CLIENT_SSL);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
 }
