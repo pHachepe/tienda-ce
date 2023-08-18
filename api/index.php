8cb5ab6a51fb390a79f164cf36de64ef3285dd0e
PeHachePe <pehachepe@uoc.edu>
2023-08-18 14:14:14 +0200

    del references to css and js

--- api/index.php
+++ api/index.php
@@ -7,9 +7,7 @@ require_once 'config/db.php';
 
 <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="styles.css">
     <script src="https://cdn.tailwindcss.com"></script>
-    <script src="script.js"></script>
 </head>
 
 <body>
