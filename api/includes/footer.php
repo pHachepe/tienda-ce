47364a75dbb5020a8974845bd2781fe1fc86b195
PeHachePe <pehachepe@uoc.edu>
2023-08-19 16:51:59 +0200

    del dark footer

--- api/includes/footer.php
+++ api/includes/footer.php
@@ -1,21 +1,21 @@
-<footer class="bg-white dark:bg-gray-900 mx-auto w-full max-w-screen-xl pb-4">
-    <hr class="py-2 border-gray-200 sm:mx-auto dark:border-gray-700 " />
+<footer class="bg-white mx-auto w-full max-w-screen-xl pb-4">
+    <hr class="py-2 border-gray-200 sm:mx-auto" />
     <div class="sm:flex sm:items-center sm:justify-between">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"><?php echo COPYRIGHT; ?></span>
+        <span class="text-sm text-gray-500 sm:text-center"><?php echo COPYRIGHT; ?></span>
         <div class="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
-            <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+            <a href="#" class="text-gray-500 hover:text-gray-900">
                 <i class="fab fa-instagram"></i>
                 <span class="sr-only"><?php echo INSTAGRAM; ?></span>
             </a>
-            <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+            <a href="#" class="text-gray-500 hover:text-gray-900">
                 <i class="fab fa-facebook"></i>
                 <span class="sr-only"><?php echo FACEBOOK; ?></span>
             </a>
-            <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+            <a href="#" class="text-gray-500 hover:text-gray-900">
                 <i class="fab fa-twitter"></i>
                 <span class="sr-only"><?php echo TWITTER; ?></span>
             </a>
-            <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+            <a href="#" class="text-gray-500 hover:text-gray-900">
                 <i class="fab fa-github"></i>
                 <span class="sr-only"><?php echo GITHUB; ?></span>
             </a>
