7e4b9910f1bf4bb66819b9ab0e9453aea355402a
PeHachePe <pehachepe@uoc.edu>
2023-10-04 17:35:43 +0200

    prueba imagenes

--- api/includes/product_box.php
+++ api/includes/product_box.php
@@ -5,7 +5,7 @@ $producto['calificacion'] = rand(1, 5);
 <div class="bg-white shadow-md p-4 flex flex-col justify-between h-full">
     <a href="index.php?producto=<?php echo $producto['id']; ?>">
         <div>
-            <img style="view-transition-name: product-image-<?= $producto['id']; ?>" src="img/<?php echo "../img/default.png"; ?>" alt="Producto" class="w-full h-auto rounded-md mb-2">
+            <img style="view-transition-name: product-image-<?= $producto['id']; ?>" src="img/<?php echo "../public/img/default.png"; ?>" alt="Producto" class="w-full h-auto rounded-md mb-2">
             <h2 class="text-lg font-semibold text-gray-800"><?php echo $producto['nombre']; ?></h2>
             <p class="text-sm text-gray-600 mb-2"><?php echo $producto['descripcion']; ?></p>
         </div>
