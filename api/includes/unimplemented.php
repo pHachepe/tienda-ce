6178a553c1e598d877e68d57cb8228284bfe39fd
PeHachePe <pehachepe@uoc.edu>
2023-11-30 12:21:00 +0100

    add uninmplemented page

---
+++ api/includes/unimplemented.php
@@ -0,0 +1,11 @@
+<?php
+
+$params = $_GET;
+$page = array_keys($params)[0];
+?>
+
+<div class="flex justify-center items-center min-h-full">
+  <h2 class="text-lg font-semibold text-gray-800">
+    La página de <?= $page ?> no está implementada todavía.
+  </h2>
+</div>
