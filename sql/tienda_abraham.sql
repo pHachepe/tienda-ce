b4844078f5eba8d2fdae4a8571655beeb9ced368
PeHachePe <pehachepe@uoc.edu>
2023-11-30 19:12:33 +0100

    fix sql db

--- sql/tienda_abraham.sql
+++ sql/tienda_abraham.sql
@@ -20,12 +20,38 @@ FLUSH PRIVILEGES;
 USE tienda_abraham;
 
 --
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) DEFAULT NULL,
+  `apellidos` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `contraseña` varchar(255) DEFAULT NULL,
+  `telefono` varchar(255) DEFAULT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `tarjeta` varchar(255) DEFAULT NULL,
+  `es_admin` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+INSERT INTO `usuarios` VALUES
+(1,'Admin','-','admin','$2y$10$e7B/evoXxX.vUKUHyP6DpeXGBmtmwXzOvErwMfY9BWD9LChnIBdLq','123456789','Calle Falsa, 321','1234567890ADM',1),
+(2,'Abraham','Fontana','abrahamfontana@gmail.com','$2y$10$e7B/evoXxX.vUKUHyP6DpeXGBmtmwXzOvErwMfY9BWD9LChnIBdLq','987654321','Calle Falsa, 123','1234567890AFR',0);
+UNLOCK TABLES;
+
+--
 -- Table structure for table `productos`
 --
 
 DROP TABLE IF EXISTS `productos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `productos` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(255) DEFAULT NULL,
@@ -35,14 +61,12 @@ CREATE TABLE `productos` (
   `imagenes` json DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `productos`
 --
 
 LOCK TABLES `productos` WRITE;
-/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
 INSERT INTO `productos` VALUES 
 (1,'Teléfono inteligente','Teléfono avanzado con cámara de alta resolución.',499.99,5,'[\"telefono-inteligente.jpg\", \"telefono-inteligente-2.jpg\"]'),
 (2,'Televisor LED','Televisor de alta definición con tecnología LED.',699.99,5,'[\"television-led.jpg\",\"television-led-2.jpg\"]'),
@@ -74,7 +98,6 @@ INSERT INTO `productos` VALUES
 (28,'Chaqueta deportiva resistente al viento','Chaqueta ligera y cortavientos para actividades deportivas.',89.99,5,NULL),
 (29,'Báscula de cocina digital','Báscula precisa para medir ingredientes al cocinar.',19.99,5,NULL),
 (30,'Botella de agua deportiva','Botella resistente y diseñada para llevar durante actividades físicas.',12.99,5,NULL);
-/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -82,24 +105,19 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `categorias`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categorias` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(255) DEFAULT NULL,
   `icono` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `categorias`
 --
 
 LOCK TABLES `categorias` WRITE;
-/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
 INSERT INTO `categorias` VALUES (1,'Electrónica','fa-tv'),(2,'Ropa','fa-tshirt'),(3,'Hogar','fa-couch'),(4,'Alimentos','fa-utensils'),(5,'Videojuegos','fa-gamepad'),(6,'Deportes','fa-futbol'),(7,'Libros','fa-book'),(8,'Música','fa-music'),(9,'Películas','fa-photo-film');
-/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -107,8 +125,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `categorias_productos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categorias_productos` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_producto` int DEFAULT NULL,
@@ -119,42 +135,13 @@ CREATE TABLE `categorias_productos` (
   CONSTRAINT `categorias_productos_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`),
   CONSTRAINT `categorias_productos_ibfk_2` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `categorias_productos`
 --
 
 LOCK TABLES `categorias_productos` WRITE;
-/*!40000 ALTER TABLE `categorias_productos` DISABLE KEYS */;
 INSERT INTO `categorias_productos` VALUES (1,1,1),(2,2,1),(3,3,1),(4,4,1),(5,5,1),(6,6,1),(7,7,1),(8,8,1),(9,9,1),(10,10,1),(11,11,2),(12,12,2),(13,13,2),(14,14,2),(15,15,2),(16,16,2),(17,17,2),(18,18,2),(19,19,2),(20,20,2),(21,21,2),(22,21,6),(23,22,1),(24,22,3),(26,23,3),(27,24,2),(28,24,3),(29,25,1),(30,25,6),(31,26,3),(33,27,1),(34,27,3),(35,28,2),(36,28,6),(37,29,1),(38,29,3),(39,30,3),(40,30,6),(41,15,6),(42,2,3),(43,4,5),(44,3,5),(45,8,6),(46,9,5),(47,27,6);
-/*!40000 ALTER TABLE `categorias_productos` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `imagenes_productos`
---
-
-DROP TABLE IF EXISTS `imagenes_productos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `imagenes_productos` (
-  `id_imagen` int NOT NULL AUTO_INCREMENT,
-  `id_producto` int DEFAULT NULL,
-  `direccion_imagen` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_imagen`),
-  KEY `id_producto` (`id_producto`),
-  CONSTRAINT `imagenes_productos_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `imagenes_productos`
---
-
-LOCK TABLES `imagenes_productos` WRITE;
-/*!40000 ALTER TABLE `imagenes_productos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `imagenes_productos` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -162,8 +149,6 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `pedidos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pedidos` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_usuario` int NOT NULL,
@@ -172,26 +157,15 @@ CREATE TABLE `pedidos` (
   `tarjeta` varchar(255) NOT NULL,
   `total` decimal(10,2) DEFAULT 0,
   PRIMARY KEY (`id`),
-  KEY `id_usuario` (`id_usuario`)
+  KEY `id_usuario` (`id_usuario`),
+  CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `pedidos`
---
-
-LOCK TABLES `pedidos` WRITE;
-/*!40000 ALTER TABLE `pedidos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pedidos` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `pedidos_productos`
 --
 
 DROP TABLE IF EXISTS `pedidos_productos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pedidos_productos` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_pedido` int DEFAULT NULL,
@@ -204,74 +178,15 @@ CREATE TABLE `pedidos_productos` (
   CONSTRAINT `pedidos_productos_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id`),
   CONSTRAINT `pedidos_productos_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `pedidos_productos`
---
-
-LOCK TABLES `pedidos_productos` WRITE;
-/*!40000 ALTER TABLE `pedidos_productos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pedidos_productos` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `sessions`
 --
 
 DROP TABLE IF EXISTS `sessions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `sessions` (
   `id` varchar(255) NOT NULL,
   `data` mediumtext NOT NULL,
   `last_accessed` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `sessions`
---
-
-LOCK TABLES `sessions` WRITE;
-/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
-INSERT INTO `sessions` VALUES ('dkqpvlfejnsvacpv8rtm22r60j','','2023-10-30 16:07:52'),('n92qm355o9vsau3hk3pb6ohtdm','','2023-11-14 05:35:31');
-/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `usuarios`
---
-
-DROP TABLE IF EXISTS `usuarios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usuarios` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(255) DEFAULT NULL,
-  `apellidos` varchar(255) DEFAULT NULL,
-  `email` varchar(255) NOT NULL,
-  `contraseña` varchar(255) DEFAULT NULL,
-  `telefono` varchar(255) DEFAULT NULL,
-  `direccion` varchar(255) DEFAULT NULL,
-  `tarjeta` varchar(255) DEFAULT NULL,
-  `es_admin` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `usuarios`
---
-
-LOCK TABLES `usuarios` WRITE;
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES
-(1,'Admin','-','admin','$2y$10$e7B/evoXxX.vUKUHyP6DpeXGBmtmwXzOvErwMfY9BWD9LChnIBdLq','123456789','Calle Falsa, 321','1234567890ADM',1),
-(2,'Abraham','Fontana','abrahamfontana@gmail.com','$2y$10$e7B/evoXxX.vUKUHyP6DpeXGBmtmwXzOvErwMfY9BWD9LChnIBdLq','987654321','Calle Falsa, 123','1234567890AFR',0);
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
--- Dump completed on 2023-11-19  9:32:47
