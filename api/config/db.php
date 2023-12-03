3efd5eac43a137df04f4e05bccec91cee2795787
PeHachePe <pehachepe@uoc.edu>
2023-12-03 22:02:17 +0100

    dix db

--- api/config/db.php
+++ api/config/db.php
@@ -1,27 +1,13 @@
 <?php
-$isLocal = $_SERVER["SERVER_NAME"] === "localhost";
-
-if ($isLocal) {
-    require_once __DIR__ . "/../../vendor/autoload.php";
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../..");
-    $dotenv->load();
-}
-
-$servername = $_ENV["TIDB_HOST"];
-$dbname = $_ENV["TIDB_NAME"];
-$username = $_ENV["TIDB_USER"];
-$password = $_ENV["TIDB_PASSWORD"];
+require_once "session_db_handler.php";
+session_start();
+$servername = 'localhost';
+$dbname = 'tienda_abraham';
+$username = 'root';
+$password = 'Abrah@M917';
 
 $conn = mysqli_init();
-
-if ($isLocal) {
-    mysqli_real_connect($conn, $servername, $username, $password, $dbname);
-} else {
-    mysqli_real_connect($conn, $servername, $username, $password, $dbname, 4000, null, MYSQLI_CLIENT_SSL);
-}
-
+mysqli_real_connect($conn, $servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-require_once "session_db_handler.php";
