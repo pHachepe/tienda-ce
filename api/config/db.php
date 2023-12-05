a2d43bee0c20364c886345dbf2cb4f15e224969b
PeHachePe <pehachepe@uoc.edu>
2023-12-05 21:50:34 +0100

    upd

--- api/config/db.php
+++ api/config/db.php
@@ -1,16 +1,13 @@
 <?php
-require_once "session_db_handler.php";
-$servername = 'gateway01.us-west-2.prod.aws.tidbcloud.com';
+$servername = 'localhost';
 $dbname = 'tienda_abraham';
-$username = '43wEyrihpbWYbWN.root';
-$password = 'xsAMBv6LE2GpXPWs';
-$port = 4000;
+$username = 'root';
+$password = 'Abrah@M917';
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, $servername, $username, $password, $dbname, 4000, null, MYSQLI_CLIENT_SSL);
-
+mysqli_real_connect($conn, $servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-
+//require_once "session_db_handler.php";
